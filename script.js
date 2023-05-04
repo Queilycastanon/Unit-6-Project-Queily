a963104434fb2e97e6b89df2cd5b307993d32389
Queilycastanon <123699341+Queilycastanon@users.noreply.github.com>
2023-05-04 14:13:43 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+// Declare your genre arrays here
+let Comedy =["https://20bedfordway.com/wp-content/uploads/2017/01/stand-up-comedian.jpg","https://www.curiouscomedy.org/assets/uploads/img/Social.jpg","https://www.hollywoodreporter.com/wp-content/uploads/2022/11/Print-Issue-32-fea_comedy1-12-Quinta-x-THR2171-MAIN-PhotoShoot-SPLASH-2022.jpg?w=2000"];
+let Books =["https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1311999970l/172763.jpg","https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781501140228/house-of-names-9781501140228_hr.jpg","https://readtothem.org/wp-content/uploads/2022/08/cover-a-boy-called-bat.jpg"];
+
+console.log(Comedy);
+// Make sure to declare your HTML elements as variables! 
+let aswer=document.querySelector(".result");
+// Submit Button 
+let submitButton = document.querySelector("button"); 
+submitButton.onclick = function() {
+}; 
+
